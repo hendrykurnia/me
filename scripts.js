3ded33a6d926031941d945a0b9043cb0a0c7a918
Hendry Kurnia <hendrykurnia@Hendrys-MacBook-Pro.local>
2025-12-16 11:21:44 -0800

    move resume file to aws

--- scripts.js
+++ scripts.js
@@ -5,3 +5,13 @@ document.querySelectorAll(".experience-header").forEach(header => {
         details.style.display = details.style.display === "block" ? "none" : "block";
     });
 });
+
+document.getElementById('downloadResume').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent default link behavior
+    const link = document.createElement('a');
+    link.href = 'https://download-hendry.s3.us-west-1.amazonaws.com/Hendry_Hendry_Resume.pdf';
+    link.download = 'Hendry_Hendry_Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
